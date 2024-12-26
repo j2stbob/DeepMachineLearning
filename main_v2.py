f2d36760d798e98fb1968443fb43dda46c64d3c2
Bobby <rotan.bohdan@gmail.com>
2024-12-26 13:00:18 +0300

    Update main_v2.py

--- main_v2.py
+++ main_v2.py
@@ -1,7 +1,7 @@
 import Deep_machine_learning_v2 as dml
 
 def main():
-    print(dml.model.training(50, 0.01))
+    (dml.model.training(50, 0.01))
     print((dml.model.predict([150, 0, 0, 1, 1, 27.5, 0.1])))
                     #Fare, Pclass_1, Pclass_2, Pclass_3, Sex, Age, Family size
                             #Select one Pclass you need and put 1 in the rest put 0
